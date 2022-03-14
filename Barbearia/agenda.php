6752f626fe8ecf06258738b2eb75be0882c0664e
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2022-03-14 12:51:40 -0300

    Projeto Site Barbearia
    Adicionando os arquivos do projeto

---
+++ Barbearia/agenda.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Menino Pimposo | Agenda</title>
+        
+    <link rel="stylesheet" href="assets/css/reset.css">
+        
+        <link rel="stylesheet" href="assets/css/cabecalho.css">
+        <link rel="stylesheet" href="assets/css/rodape.css">
+
+    </head>
+    <body>
+        <header>
+            <div class="logo"><a href="index.php"><img src="assets/img/boy.png" width="50px" height="50px"><h1>MENINO PIMPOSO</h1></a></div>
+            <nav class="navegador">
+                <ul class="navegador-links">
+                    <li>
+                        <a href="index.php" target="_blanc">Home</a>
+                    </li>
+                    <li>
+                        <a href="sobre.html" target="_blanc">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="servicos.html" target="_blanc">Serviços</a>
+                    </li>
+                    <li>
+                        <a href="contatos.php" target="_blanc">Contatos</a>
+                    </li>
+                    <li>
+                        <a href="agenda.php" target="_blanc">Agenda</a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa-brands fa-facebook-square fa-lg"></i></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa-brands fa-instagram fa-lg"></i></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <div>AGENDA</div>
+        <div>CADASTRO</div>
+        <div>RESERVE UM HORÁRIO</div>
+        <footer>
+            <div class="conteudo-rodape">
+                <div class="rodape-texto">
+                    Menino pimposo barbearia
+                    <span>@2022</span>
+                </div>
+                
+                <div class="rodape-links">
+                    <a href=""><i class="fa-brands fa-facebook-square fa-lg"></i></a>
+                    <a href=""><i class="fa-brands fa-instagram fa-lg"></i></a>
+                </div>
+            </div>
+        </footer>
+        <?php
+        // put your code here
+        ?>
+        
+        <!--Icons-->
+        <script src="https://kit.fontawesome.com/b8158c0067.js" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ Barbearia/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>Menino Pimposo</title>
+        
+        <link rel="stylesheet" href="assets/css/reset.css">
+        <link rel="stylesheet" href="assets/css/homeCSS.css">
+        <link rel="stylesheet" href="assets/css/cabecalho.css">
+        <link rel="stylesheet" href="assets/css/rodape.css">
+        
+        
+        <link rel="stylesheet" href="assets/css/bootstrap-5.1.3-dist/css/bootstrap-grid.css">
+    </head>
+    <body>
+        <header>
+            <div class="logo logo-size"><a href="index.php"><img src="assets/img/boy.png" width="50px" height="50px"><h1>MENINO PIMPOSO</h1></a></div>
+            <nav class="navegador">
+                <ul class="navegador-links">
+                    <li>
+                        <a href="index.php" target="_blanc">Home</a>
+                    </li>
+                    <li>
+                        <a href="sobre.html" target="_blanc">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="servicos.html" target="_blanc">Serviços</a>
+                    </li>
+                    <li>
+                        <a href="contatos.php" target="_blanc">Contatos</a>
+                    </li>
+                    <li>
+                        <a href="agenda.php" target="_blanc">Agenda</a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa-brands fa-facebook-square fa-lg"></i></a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fa-brands fa-instagram fa-lg"></i></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        
+        <div class="principal">
+        </div>
+
+        <div class="centered welcome">
+            <small class="small-top">MENINO PIMPOSO</small>
+            BEM-VINDOS
+            <small class="small-bottom">BARBEARIA</small>
+        </div>   
+
+        <div class="rest"></div>
+
+        <div class="conteudo">
+            <div class="wrapper left">
+                <img src="assets/img/Barber-pic (1).jpg" alt="Foto da barbearia" class="imagem-grid">
+                <img src="assets/img/Barber-pic (2).jpg" alt="Foto da barbearia" class="imagem-grid">
+                <img src="assets/img/Barber-pic (3).jpg" alt="Foto da barbearia" class="imagem-grid">
+                <img src="assets/img/Barber-pic (4).jpg" alt="Foto da barbearia" class="imagem-grid">
+            </div>
+
+            <div class="wrapper right horarios">
+                <h3>BARBEARIA MENINO PIMPOSO</h3>
+                <span>HORÁRIOS DE FUNCIONAMENTO</span>
+                <div class="wrapper-texto">
+                    <p>Segunda a Sexta .................... 8h às 18h</p>
+                    <p>Sábado, domingo e feriados ............ 10h </p>
+                </div>
+                
+                <span>LOCALIZAÇÃO</span>
+                <div class="wrapper-texto">
+                    <a href="https://www.google.com/maps/place/Barbearia+Dom+Ribeiro/@-15.7883176,-47.9409155,15z/data=!4m5!3m4!1s0x0:0x7bdd7121e34b4b21!8m2!3d-15.7883176!4d-47.9409155" target="_blanc"><p>SRES Cruzeiro Center Bloco A Loja 121 - Cruzeiro Velho, Brasília - DF, 70640-515</p><a>
+                </div>
+                <div class="botoes">
+                    <span class="botao">AGENDE<br> UM HORÁRIO</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="conteudo">
+            <div class="column side">
+                CONTEUDO 1
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloremque eveniet ipsum quasi sint placeat ab in similique unde. Voluptatum odio aspernatur ullam accusantium! Dicta veniam aliquam maxime culpa dolores?</p>
+                
+            </div>
+
+            <div class="column middle">
+                CONTEUDO 2
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A qui inventore, obcaecati corporis molestiae corrupti, odio quis repellendus veritatis doloribus dolores. Similique modi id eligendi eveniet, corporis necessitatibus recusandae voluptate.</p>
+            </div>
+
+            <div class="column side">
+                CONTEUDO 3
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis illum aliquid esse natus, adipisci mollitia debitis harum laborum temporibus dolor a, explicabo est, placeat in ipsa blanditiis quis. Nam, itaque?</p>
+            </div>
+        </div>
+
+        <?php
+        ?>
+        
+
+        <footer>
+            <div class="conteudo-rodape">
+                <div class="rodape-texto">
+                    Menino pimposo barbearia
+                    <span>@2022</span>
+                </div>
+                
+                <div class="rodape-links">
+                    <a href=""><i class="fa-brands fa-facebook-square fa-lg"></i></a>
+                    <a href=""><i class="fa-brands fa-instagram fa-lg"></i></a>
+                </div>
+            </div>
+        </footer>
+
+        <!--Icons-->
+        <script src="https://kit.fontawesome.com/b8158c0067.js" crossorigin="anonymous"></script>
+    </body>
+</html>
