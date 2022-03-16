2ac0d1dfd5583dd95ac97aebcaad1fd967c6cadd
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2022-03-16 11:55:20 -0300

    Alterações feitas em aula

--- Barbearia/agenda.php
+++ Barbearia/agenda.php
@@ -9,10 +9,11 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>Menino Pimposo | Agenda</title>
         
-    <link rel="stylesheet" href="assets/css/reset.css">
+        <link rel="stylesheet" href="assets/css/reset.css">
         
         <link rel="stylesheet" href="assets/css/cabecalho.css">
         <link rel="stylesheet" href="assets/css/rodape.css">
+        <link rel="stylesheet" href="assets/css/agendaCSS.css">
 
     </head>
     <body>
@@ -33,7 +34,7 @@ and open the template in the editor.
                         <a href="cadastro.php" target="_blanc">Cadastro</a>
                     </li>
                     <li>
-                        <a href="agenda.php" target="_blanc">Agenda</a>
+                        <a href="agenda.php" target="_blanc">Agendamento</a>
                     </li>
                     <li>
                         <a href=""><i class="fa-brands fa-facebook-square fa-lg"></i></a>
@@ -44,51 +45,57 @@ and open the template in the editor.
                 </ul>
             </nav>
         </header>
-        <div>AGENDA</div>
-        <div>CADASTRO</div>
-        <div>
-            <h2>RESERVE UM HORÁRIO</h2>
-
+        <h2 class="titulo-pagina">RESERVE UM HORÁRIO</h2>
+        <div class="formulario">
             <form>
-                <label for="nome">Nome do cliente</label><br>
-                <input type="text" id="nome" name="nome" placeholder="nome do cliente"><br>
-                <label for="telefone">Telefone</label><br>
-                <input type="tel" id="telefone" name="telefone" placeholder="00 00000-0000"><br>
-                <label for="email">E-mail</label><br>
-                <input type="email" id="email" name="email" placeholder="exemplo@gmail.com"><br>
-                <label for="func">Nome do funcionário</label><br>
-                <select id="func" name="func">
-                    <option value="func">Escolha o funcionário</option>    
-                    <option value="func">Chico Alicate</option>
-                    <option value="func">Aranha</option>
-                    <option value="func">Zé Martelo</option>
-                    <option value="func">Morsa</option>
-                    <option value="func">Pedro Machado</option>
-                    <option value="func">Tonho Couve-flor</option>
-                    <option value="func">Cabeçudo</option>
-                    <option value="func">Zé bom de perna</option>
-                    <option value="func">Cilindro</option>
-                    <option value="func">Homem-Montanha</option>
-                    <option value="func">Fantasma</option>
-                    <option value="func">Sombra</option>
-                    <option value="func">Borracha</option>
-                    <option value="func">Tenório Tesoura</option>
-                </select><br>
-                <label for="serv">Serviço</label><br>
-                <select id="serv" name="serv"> <!-- Adicionar uma maneira de conseguir escolher mais de um serviço, com checkbox, por exemplo -->
-                    <option value="serv">Escolha o serviço</option>
-                    <option value="serv">Cabelo</option>
-                    <option value="serv">Barba</option>
-                    <option value="serv">Bigode</option>
-                    <option value="serv">Sobrancelha</option>
-                    <option value="serv">Cavanhaque</option>    
-                </select><br>
-                <label for="data">Data de agendamento</label><br>
-                <input type="date" id="data" name="data"><br>
-                <label for="time">Horário</label><br>
-                <input type="time" id="time" name="time"><br><br>
-                <input type="submit" value="Enviar">
-                <input type="reset" value="Apagar">
+                <fildset>
+                    <legend>Agendamento</legend>
+                    <label for="nome" class="input-titulo">Nome do cliente</label><br>
+                    <input type="text" id="nome" name="nome" placeholder="nome do cliente"><br><br>
+                    <label for="telefone" class="input-titulo">Telefone</label><br>
+                    <input type="tel" id="telefone" name="telefone" placeholder="00 00000-0000"><br><br>
+                    <label for="email" class="input-titulo">E-mail</label><br>
+                    <input type="email" id="email" name="email" placeholder="exemplo@gmail.com"><br><br>
+                    <label for="func" class="input-titulo">Nome do funcionário</label><br>
+                    <select id="func" name="func">
+                        <option value="func">Escolha o funcionário</option>    
+                        <option value="func">Chico Alicate</option>
+                        <option value="func">Aranha</option>
+                        <option value="func">Zé Martelo</option>
+                        <option value="func">Morsa</option>
+                        <option value="func">Pedro Machado</option>
+                        <option value="func">Tonho Couve-flor</option>
+                        <option value="func">Cabeçudo</option>
+                        <option value="func">Zé bom de perna</option>
+                        <option value="func">Cilindro</option>
+                        <option value="func">Homem-Montanha</option>
+                        <option value="func">Fantasma</option>
+                        <option value="func">Sombra</option>
+                        <option value="func">Borracha</option>
+                        <option value="func">Tenório Tesoura</option>
+                    </select><br><br>
+                    <div id="serv">
+                        <h3 class="input-titulo">Serviços</h3>
+                        <input type="checkbox" id="cabelo" name="cabelo" value="cabelo" class="checkmark">
+                        <label for="cabelo">Cabelo</label><br>
+                        <input type="checkbox" id="barba" name="barba" value="barba" class="checkmark">
+                        <label for="barba">Barba</label><br>
+                        <input type="checkbox" id="bigode" name="bigode" value="bigode" class="checkmark">
+                        <label for="bigode">Bigode</label><br>
+                        <input type="checkbox" id="sobrancelha" name="sobrancelha" value="sobrancelha" class="checkmark">
+                        <label for="sobrancelha">Sobrancelha</label><br>
+                        <input type="checkbox" id="cavanhaque" name="cavanhaque" value="cavanhaque" class="checkmark">
+                        <label for="cavanhaque">Cavanhaque</label>  
+                    </div><br><br>
+                    <label for="data" class="input-titulo">Data de agendamento</label><br>
+                    <input type="date" id="data" name="data"><br><br>
+                    <label for="time" class="input-titulo">Horário</label><br>
+                    <input type="time" id="time" name="time"><br><br>
+                    <div class="botoes-formulario">
+                        <input class="enviar botao" type="submit" value="Enviar">
+                        <input class="apagar botao" type="reset" value="Apagar">
+                    </div>
+                </fildset>
             </form>
         </div>
         <footer>
@@ -113,5 +120,12 @@ and open the template in the editor.
         
         <!--Icons-->
         <script src="https://kit.fontawesome.com/b8158c0067.js" crossorigin="anonymous"></script>
+
+        <script>
+            var form = document.getElementById("nome").clientWidth;
+            form += 2;
+            var largura = form + "px";
+            document.getElementById("func").style.width = largura;
+        </script>
     </body>
 </html>
